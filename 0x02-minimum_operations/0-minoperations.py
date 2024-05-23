27c2fbf8c7eab4eb8397802af442fdf36f7a8890
tha-orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-05-23 23:51:56 +0100

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,20 +2,38 @@
 """minimum operations"""
 
 
-def minOperations(n: int) -> int:
+# def minOperations(n: int) -> int:
+#     """calculates the fewest number of operations
+#     needed to result in exactly n H"""
+#     if n <= 1:
+#         return 0
+
+#     inp = "H"
+#     mul = 1
+#     result = "H"
+#     moves = 0
+#     while len(result) < n:
+#         if n % len(result) == 0:
+#             mul = len(result)
+#             moves += 1
+#         result += inp * mul
+#         moves += 1
+#     return moves
+
+
+def minOperations(n):
     """calculates the fewest number of operations
     needed to result in exactly n H"""
     if n <= 1:
         return 0
 
-    inp = "H"
-    mul = 1
-    result = "H"
-    moves = 0
-    while len(result) < n:
-        if n % len(result) == 0:
-            mul = len(result)
-            moves += 1
-        result += inp * mul
-        moves += 1
-    return moves
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
